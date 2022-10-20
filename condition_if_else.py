a8d2a09b396a294e8a1181a95fa13febfe4654a8
Tariqul Islam <islamtariqul565@gmail.com>
2022-10-21 01:21:03 +0600

    if else

---
+++ condition_if_else.py
@@ -0,0 +1,10 @@
+age: int = 15
+
+if age >= 18:
+    print('you are eligible to register')
+elif age >= 16:
+    print('you are alomost')
+else:
+    print('you are not eligible to register')
+
+print('Thanks for visiting')
