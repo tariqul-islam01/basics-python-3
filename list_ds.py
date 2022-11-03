548d4f77c61f93888610f8dbb5a968894557dfc2
Tariqul Islam <islamtariqul565@gmail.com>
2022-11-04 01:24:41 +0600

    lists

---
+++ list_ds.py
@@ -0,0 +1,12 @@
+fruits: list[str] = ['apple', 'banana', 'jack fruit', 'mango']
+
+print(fruits)
+
+print(f'Fruits length {len(fruits)}')
+for fruit in fruits:
+    print(fruit)
+
+
+fruits.pop()
+
+print(fruits)
