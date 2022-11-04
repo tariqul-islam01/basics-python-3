b090fabc3ca1a8e338a4e6adbcf6e355a662e32f
Tariqul Islam <islamtariqul565@gmail.com>
2022-11-05 01:27:14 +0600

    lists

--- list_ds.py
+++ list_ds.py
@@ -7,6 +7,36 @@ for fruit in fruits:
     print(fruit)
 
 
-fruits.pop()
+removedFruit = fruits.pop()
 
+print(f'removed fruits {removedFruit}')
+
+print(fruits)
+
+fruits.append('avocado')
+print(fruits)
+
+fruits.remove('banana')
+
+print(f'fruits after removing banana {fruits}')
+
+fruits.insert(1, 'banana')
+
+print(f'insert banana in index 1 {fruits}')
+
+
+if 'banana' in fruits:
+    print('Yes banana in the list')
+else:
+    print('No banana is not in the list')
+
+print(f'Before sort: {fruits}')
+fruits.sort()
+print(f'After sort: {fruits}')
+
+fruits.reverse()
+print(f'After reverse: {fruits}')
+
+removed_fruits_by_index = fruits.pop(2)
+print(f'Removed index 2 : {removed_fruits_by_index}')
 print(fruits)
