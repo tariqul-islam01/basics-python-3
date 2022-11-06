0d7353d58904e303eeb59846ce597322c2876e86
Tariqul Islam <islamtariqul565@gmail.com>
2022-11-07 01:36:36 +0600

    function return

--- function.py
+++ function.py
@@ -2,11 +2,21 @@ def printLangs(name: str) -> None:
     print(f'This language is : {name}')
 
 
+def getSubject(name: str) -> str:
+    return name
+
+
 def main() -> None:
     langs: list[str] = ['Python', 'Java', 'Java script', 'ECMA script']
 
     for lang in langs:
-        printLangs(lang)
+        # printLangs(lang)
+        if 'Java' in getSubject(lang):
+            print(f'{lang} in the list')
+            break
+        else:
+            print('Java is not in the list')
+
     print('Thanks for the time')
 
 
