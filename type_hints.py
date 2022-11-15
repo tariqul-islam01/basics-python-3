dadfe5fce5bd330b41d16bc570767dcff75359be
Tariqul Islam <islamtariqul565@gmail.com>
2022-11-16 03:15:42 +0600

    type

--- type_hints.py
+++ type_hints.py
@@ -1,9 +1,21 @@
+
 my_score: int = 34
 
 print(f'my score is {my_score}')
 
-my_score = 'False'
+my_score = 54
 
 print(f'my score is {my_score}')
 
 # use plugin mypy
+
+str_group = dict[str, str | int]
+
+MAKBOOK: str_group = {
+    "name": "Macbook air",
+    "chip": "M1",
+    "memory": "16 GB",
+    "release": 2020
+}
+
+print(MAKBOOK)
