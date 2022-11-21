749c21b5a2ac3b4d8f1822822936c74b51828a38
Tariqul Islam <islamtariqul565@gmail.com>
2022-11-22 01:48:59 +0600

    decorators

--- decorator.py
+++ decorator.py
@@ -1,5 +1,7 @@
+from __future__ import annotations
 from enum import Enum, auto
 from datetime import datetime
+from typing import final
 
 
 class Role(Enum):
@@ -21,6 +23,8 @@ class Person:
         return f'{self.fname} {self.lname}'
 
 
+# no inheritance possible
+@final
 class Staff(Person):
     def __init__(self, fname: str, lname: str, staff_id: int, role: Role) -> None:
         super().__init__(fname, lname)
@@ -43,6 +47,31 @@ class Staff(Person):
         # return super().__str__()
         return f'Name is: {self.fullname}'
 
+    @classmethod
+    def new(cls, person: Person, staff_id: int, role: Role) -> Staff:
+        return cls(person.fname, person.lname, staff_id, role)
+
+    @property
+    def joined_date(self) -> str:
+        return f"{self._date_joined.strftime('%B %d, %Y')}"
+
+    @property
+    def salary(self) -> float:
+        return self.__salary
+
+    @salary.setter
+    def salary(self, amt: float) -> None:
+        if self.role == Role.ASSOCIATE and amt < 15:
+            print('Error! Associate can not have salary less than $15/hr')
+        elif self.role == Role.SUPERVISOR and amt < 20:
+            print('Error! Supervisor can not have salary less than $20/hr')
+        elif self.role == Role.MANAGER and amt < 25:
+            print('Error! Manager can not have salary less than $25/hr')
+        else:
+            self.__salary = amt
+            print(
+                f"Staff Details: Name: {self.fullname} has salary ${self.salary}/hr")
+
 
 p1 = Person('John', 'Doe')
 print(p1)
@@ -50,3 +79,10 @@ print(p1.fullname)
 
 employee1 = Staff('Tariqul', 'Islam', 1, Role.MANAGER)
 print(employee1)
+
+employee2 = Staff.new(p1, 1234, Role.SUPERVISOR)
+print(employee2)
+print(employee2.joined_date)
+print(employee2.salary)
+employee2.salary = 25
+print(employee2.salary)
